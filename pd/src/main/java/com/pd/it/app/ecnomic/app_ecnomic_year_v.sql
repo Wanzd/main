106bff71726da48344c2c31e1fa78726fb174e6a
panda_zdwan <panda_zdwan@hotmail.com>
2019-01-30 20:56:57 +0800

    feat:经济年度数据汇总

--- pd/src/main/java/com/pd/it/app/ecnomic/app_ecnomic_year_v.sql
+++ pd/src/main/java/com/pd/it/app/ecnomic/app_ecnomic_year_v.sql
@@ -5,4 +5,4 @@ select year(event_date) as year,
 	greatest(sum(ifnull(output,0)-ifnull(income,0)),0) as gap,
 	greatest(sum(ifnull(income,0)-ifnull(output,0)),0) as rest
 from app_ecnomic_t t0
-group by year(event_date),month(event_date)
+group by year(event_date),month(event_date) with rollup
